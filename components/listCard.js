b1fe20d0b3952f304f603518567f009f0426b7b6
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-08-19 16:01:38 -0300

    Create listCard.js

---
+++ components/listCard.js
@@ -0,0 +1,55 @@
+const cardList = d3.select("#list-card").append("ul");
+
+export function listCard (data) {
+    
+    data.forEach((d) => {    
+        
+             
+        cardList.append("li")
+        .append("button")
+        .attr("commandfor", d._id)
+        .attr("command", "show-modal")
+        .attr("class", "button-list")
+        .text(d.termo);
+
+        const cardListDialog = cardList.append("dialog")
+                                        .attr("id", d._id);
+
+        cardListDialog.append("button")
+        .attr("commandfor", d._id)
+        .attr("command", "close")
+        .attr("class", "button-close")
+        .text("Close");                                            
+  
+        const cardListDialogForm = cardListDialog.append("form");
+
+        cardListDialogForm.append("label")
+        .attr("for", "termo")
+        .text("Termo");
+
+        cardListDialogForm.append("textarea")
+        .attr("id", "termo")
+        .attr("name", "termo")
+        .attr("rows", 5)
+        .attr("cols", 33)
+        .text(d.termo);
+
+        cardListDialogForm.append("label")
+        .attr("for", "significado")
+        .text("Significado");
+
+        cardListDialogForm.append("textarea")
+        .attr("id", "significado")
+        .attr("name", "significado")
+        .attr("rows", 5)
+        .attr("cols", 33)
+        .text(d.significado);
+
+        cardListDialogForm.append("br");
+
+        cardListDialogForm.append("input")
+        .attr("type", "submit")
+        .attr("value", "Editar");    
+
+    })    
+}
