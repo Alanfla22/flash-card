730446176f0c7b38e5a2c0630de9b86d3d068667
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-08-19 15:59:39 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+import { viewCard } from "./components/viewCard.js";
+import { listCard } from "./components/listCard.js";
+
+d3.json("https://fast-mongo-test-2.onrender.com").then((data) => {
+
+    const obj = JSON.parse(data);
+
+    console.log(obj);
+
+    viewCard(obj);
+    listCard(obj);  
+
+
+});
+
+
+
+
+document.getElementById("view-card-tab").click();
+
+const formElem = document.getElementById("form");
+
+formElem.addEventListener("formdata", (e) => {
+
+  // Get the form data from the event object
+  const data = e.formData;
+
+  // Submit the data via fetch()
+  fetch("https://fast-mongo-test-2.onrender.com", {
+    method: "POST",
+    body: data,
+  });
+});
